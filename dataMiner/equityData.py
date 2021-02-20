3a8a11f7469bfcfd86e3e45c8b0bc1aa628efc51
unknown <jsc126@case.edu>
2021-02-20 00:21:29 -0600

    create equity data scraper

---
+++ dataMiner/equityData.py
@@ -0,0 +1,68 @@
+'''jchan 2021
+
+A routine to pull equity data
+
+REVISION HISTORY
+    2/21 - Initial implementation
+'''
+import requests 
+
+appToken = os.environ.get('TradierAppToken')
+accessToken = os.environ.get('TradierAccessToken')
+
+INTRADAY_URL = "https://sandbox.tradier.com/v1/markets/timesales"
+CSV_COLUMNS = ['date','time', 'open', 'high', 'low', 'close', 'volume', 'vwap']
+
+'''Obtain quote data between start-endDate
+    @param interval: 1,5, or 15 min
+    @param startDate: date in YYYY-MM-DD
+    @param showMarketHoursDataOnly: False to display after hours data'''
+def getStockData(ticker, startDate, endDate, interval, showMarketHoursDataOnly = True):
+    if showMarketHoursDataOnly:
+        startTime = "09:30"
+        endTime = "16:00"
+    else:
+        startTime = "06:30"
+        endTime = "18:59"
+
+    if interval not in {1,5,15}:
+        return
+
+    fqStartDate = startDate + " " + startTime
+    fqEndDate = endDate + " " + endTime
+
+    headersObj = {
+        'Accept' : 'application/json',
+        'Authorization' : 'Bearer' + accessToken
+    }
+
+    payload = {
+        'symbol' : ticker,
+        'interval' : interval,
+        'start' : fqStartDate,
+        'end' : fqEndDate,
+        'session-filter' : ('all', 'open')[showMarketHoursDataOnly]
+    }
+    return requests.get(INTRADAY_URL, params = payload, headers = headersObj).json()
+
+def writeCSV(ticker, startDate, endDate, interval = 15, showMarketHoursDataOnly = True):
+    filename = "equityData/" + ticker + startDate + "_" + endDate + ".csv"
+
+    with open(filename, 'w', newLine = '') as file:
+        data = getStockData(ticker, startDate, endDate, interval, showMarketHoursDataOnly)
+        writer = csv.writer(file)
+        writer.writerow(CSV_COLUMNS)
+        try:
+            dataArr = data['series']['data']
+            dataRow = []
+            for dataPoint in len(dataArr):
+                fullDate = dataArr[dataPoint]['time']
+                dataRow.append(fullDate.split("T")[0]) #date
+                dataRow.append(fullDate.split("T")[1]) #time
+                dataRow.append(dataArr[dataPoint]['open'])
+                dataRow.append(dataArr[dataPoint]['high'])
+                dataRow.append(dataArr[dataPoint]['low'])
+                dataRow.append(dataArr[dataPoint]['close'])
+                dataRow.append(dataArr[dataPoint]['volume'])
+                dataRow.append(dataArr[dataPoint]['vwap'])
+            writer.writerow(dataRow)
